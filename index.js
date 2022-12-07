08d86e2b5e24dfa4e82a0c10417328bbef5284e5
Larisa Frolova <larisa@frolova.org>
2022-12-07 11:06:27 -0800

    dynamically inserted js variables of cities and contries into a URL

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const hostCountry = document.getElementById("hcountry").value;
 const hostDate = document.getElementById("hdate").value;
 const hostTime = document.getElementById("htime").value;
 
-const locatCity = document.getElementById("lcity").value;
-const locatCountry = document.getElementById("lcountry").value;
+const localCity = document.getElementById("lcity").value;
+const localCountry = document.getElementById("lcountry").value;
 
     document.getElementById("block-result").style.visibility = "visible";
 
-    var url = "https://timezone.abstractapi.com/v1/current_time/?api_key=f0b51a94ee7e42b7ab5db8d1bade8045&location=Oxford, United Kingdom"
+    var url = `https://timezone.abstractapi.com/v1/convert_time/?api_key=f0b51a94ee7e42b7ab5db8d1bade8045&&base_location=${hostCity}, ${hostCountry}&base_datetime=2022-12-08 07:00:00&target_location=${localCity}, ${localCountry}`
     
     httpGetAsync(url);
   }
@@ -23,13 +23,22 @@ const locatCountry = document.getElementById("lcountry").value;
     document.getElementById("block-result").style.visibility = "hidden";
   }
 
-  function httpGetAsync(url, callback) {
+  function httpGetAsync(url) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-        callback(xmlHttp.responseText);
-    }
+    // xmlHttp.onreadystatechange = function() {
+    //     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+    //     callback(xmlHttp.responseText);
+    // }
     xmlHttp.open("GET", url, true); // true for asynchronous
+
+    xmlHttp.responseType = 'json';
+
+    xmlHttp.onload = function() {
+        const userInput = xmlHttp.response;
+        console.log("user input is "+userInput);
+    };
+    
+    
     xmlHttp.send(null);
 }
 
