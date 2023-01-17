5c3eb035ac89fe0ffa36eca88d512690a4ea6461
Larisa Frolova <larisa@frolova.org>
2023-01-17 12:05:33 -0800

    added a validation to the city; added paragraphs to the html for validation city, country, date and time

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const hostTime = document.getElementById("htime").value;
 const localCity = document.getElementById("lcity").value;
 const localCountry = document.getElementById("lcountry").value;
 
+// validate(hostCity, hostCountry, hostDate, hostTime);
+validateCity (hostCity);
+
     document.getElementById("block-result").style.visibility = "visible";
 
     var url = `https://timezone.abstractapi.com/v1/convert_time/?api_key=f0b51a94ee7e42b7ab5db8d1bade8045&&base_location=${hostCity}, ${hostCountry}&base_datetime=2022-12-08 07:00:00&target_location=${localCity}, ${localCountry}`
@@ -49,7 +52,7 @@ const localCountry = document.getElementById("lcountry").value;
         //console.log('text after .TEXT');
       }
       else {
-        alert('something is wrong!!!!');
+        alert('400 Bad request!!!!');
       }
     };
 
@@ -58,6 +61,91 @@ const localCountry = document.getElementById("lcountry").value;
     };
     
     xmlHttp.send(null);
+}
+
+// function validate() {
+//   var formsCollection = document.forms;
+//   const validText = document.getElementsByClassName("validation-text");
+//   for(var i=0;i<formsCollection.length;i++) {
+//     console.log('validText is '+validText);
+//     console.log('I am in the for loop');
+//     let city = formsCollection[i]["city"].value;
+//     console.log('city is '+city);
+//     if(city === "") {
+//       console.log('i am in in the 1 if statement for loop');
+//       validText.style['visibility'] = 'visible';
+//       // formsCollection[i]["validation-text"].style.visibility = "visible";
+//     // alert( "Please provide your city!" );
+//     formsCollection[i]["city"].focus();
+//     // validText.style.visibility = 'hidden';
+//     return false;
+//     }
+//     validText.style['visibility'] = 'hidden';
+//  let country = formsCollection[i]["country"].value;
+//  console.log('country is '+country);
+//  if( country === "") {
+//   console.log('i am in in the 2 if statement for loop');
+//     alert( "Please provide your country!");
+//     formsCollection[i]["country"].focus();
+//     return false;
+//  }
+//   }
+  
+// //  if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
+// //     document.myForm.Zip.value.length != 5 ) {
+    
+// //     alert( "Please provide a zip in the format #####." );
+// //     document.myForm.Zip.focus() ;
+// //     return false;
+// //  }
+// //  if( document.myForm.Country.value == "-1" ) {
+// //     alert( "Please provide your country!" );
+// //     return false;
+// //  }
+//  return( true );
+// }
+
+
+function validateCity (city) {
+  //var formsCollection = document.forms;
+  //let validText = document.getElementsByClassName("validation-text");
+ // for(var i=0;i<formsCollection.length;i++) {
+    city = document.forms['myForm'].city.value;
+    //country = formsCollection[i]["country"].value;
+    if(city === "" || city === null) {
+      alert("returning false to the validate city!")
+      return false;
+      //alert ('validate is working good!');
+      // for (let item of validText) {
+      //   console.log("validText is "+validText);
+      //   console.log('item is '+item);
+      //   item.style.visibility = 'visible';
+      //   alert( "Please provide your user data!" );
+      //   formsCollection[i]["city"].focus();
+      //   item.style.visibility = 'hidden';
+      //   return false;
+      // }
+      //validText.style.visibility = 'hidden';
+    }
+    // }
+  }
+
+
+function validateCountry (country) {
+  country = document.forms['myForm'].country.value;
+  if(country === "" || country === null) {
+    alert("returning false to the validate country!")
+      return false;
+  }
+  //alert ("it is working! validate with no arguments");
+}
+
+function validateDate (date) {
+  alert ("it is working! validate with no arguments");
+}
+
+function validateTime (time) {
+  alert ("it is working! validate with no arguments");
 }
 
 
